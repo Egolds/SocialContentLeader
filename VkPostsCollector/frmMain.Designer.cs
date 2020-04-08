5cc1d3a2f58363abfbc909ec24a97e5b2e53c1f5
Egolds <47989977+Egolds@users.noreply.github.com>
2020-04-08 19:09:46 +0200

    rename form component

--- VkPostsCollector/frmMain.Designer.cs
+++ VkPostsCollector/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VkPostsCollector
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(767, 443);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сборщик постов в ВК";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- VkPostsCollector/frmMain.cs
+++ VkPostsCollector/frmMain.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VkPostsCollector
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
